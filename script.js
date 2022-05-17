dc7648b7efc774cc04b5739a5dc78653b7d9e066
Filhafirahh <filhafirah2000016071@webmail.uad.ac.id>
2022-05-17 12:06:40 +0700

    update

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const isi = {
-    judul:'Portfolio 1',
-    sapa:'Hello...',
-    h2:'Welcome to portofolio website',
-    pribadi:'Saya adalah mahasiswi semester 4, prodi Sistem Informasi di Universitas Ahmad Dahlan. Saya lahir di kota Bantul tepatnya pada tanggal 9 bulan September tahun 2002. Dulu saya sekolah menengah kejuruan di kota Makassar yaitu di SMKN 1 Takalar mengambil jurusan Teknik Komputer Jaringan. Saya pernah melakukan PKL atau praktek kerja lapangan pada sebuah tempat service laptop yg berada di kota Makassar yaitu di Mall MTC Makassar. Dari situ saya banyak mendapat pengalaman seperti mengganti baterai, mengganti keyboard tanam dan lain sebagainya.',
-    
-
-
-    
-}
-const vm = new Vue({
-    el: ".global",
-    data: isi
+let app = Vue.createApp({
+    data() {
+        return {
+            articles: {},
+            markdown: null
+        }
+    },
+    methods: {
+        getArticleData() {
+            axios
+                .get(
+                    "https://raw.githubusercontent.com/Filhafirahh/tekweb2022/main/data.json"
+                )
+                .then((res) => {
+                    this.articles = res.data;
+                    console.log(res.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    },
+    beforeMount() {
+        this.getArticleData()
+    }
 })
+app.mount('#app');
